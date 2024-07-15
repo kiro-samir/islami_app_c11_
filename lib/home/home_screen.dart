94f5bfdbf47db16012eeddb3034e0b203c13f473
Kimo Store <kirosamir359@gmail.com>
2024-07-15 10:41:39 +0200

    Init Project . Added empty home screen.

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = "homeScreen";
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
